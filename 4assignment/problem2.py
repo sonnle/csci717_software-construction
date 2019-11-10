5ff0acbeb3642e66e74020ddb9b5bee179dc81fc
sonnle <son.n.le@hotmail.com>
2019-11-10 11:02:41 -0600

    assignment4, problem2

---
+++ 4assignment/problem2.py
@@ -0,0 +1,46 @@
+import random
+import time
+
+def main():
+    a_time = 0.0
+    b_time = 0.0
+    for i in range(1000):
+        search_list = [random.randint(0,200000) for i in range(200000)]
+        value = random.randint(0, 200000)
+
+        start_timea = time.time()
+        finda(value, search_list)
+        a_time += time.time() - start_timea
+
+        start_timeb = time.time()
+        findb(value, search_list)
+        b_time += time.time() - start_timeb
+
+    print str(a_time / 100)
+    print str(b_time / 100)
+
+    // a_time = .45s/run
+    // b_time = .20s/run
+
+def finda(value, search_list):
+    found = False
+    i = 0
+    while(not found and i < len(search_list)):
+        if search_list[i] == value:
+            found = True
+        else:
+            i += 1
+    return found
+
+
+def findb(value, search_list):
+    init_value = search_list[len(search_list) - 1]
+    search_list[len(search_list) - 1] = value
+    i = 0
+    while(search_list[i] != value):
+        i += 1
+    search_list[len(search_list) - 1] = init_value
+    return i < len(search_list) - 1 or value == init_value
+
+if __name__ == '__main__':
+    main()
